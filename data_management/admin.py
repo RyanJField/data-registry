5bbfb61a89b807a4a00622855e98b136998d546d
Jonathan Hollocombe <Jonathan.Hollocombe@ukaea.uk>
2020-07-10 10:01:42 +0100

    Updating admin list pages to display more information about each object.

--- data_management/admin.py
+++ data_management/admin.py
@@ -1,26 +1,8 @@
 from django.contrib import admin
-from django.forms import ModelForm
-from django.contrib.contenttypes.admin import GenericStackedInline
-from django.utils.html import format_html
-from django.urls import reverse
 
 from . import models
 
 admin.site.site_header = 'SCRC Data Management'
-
-
-class IssueForm(ModelForm):
-    def save(self, **kwargs):
-        self.instance.updated_by = self.updated_by
-        return super().save(**kwargs)
-
-
-class IssueInline(GenericStackedInline):
-    model = models.Issue
-    form = IssueForm
-
-    def get_extra(self, request, obj=None, **kwargs):
-        return 0
 
 
 class BaseAdmin(admin.ModelAdmin):
@@ -32,40 +14,41 @@ class BaseAdmin(admin.ModelAdmin):
         return super().save_model(request, obj, form, change)
 
 
-class ObjectAdmin(BaseAdmin):
-    inlines = (IssueInline,)
-
-    def save_related(self, request, form, formsets, change):
-        for formset in formsets:
-            for f in formset:
-                f.updated_by = request.user
-        return super().save_related(request, form, formsets, change)
-
-
 class IssueAdmin(BaseAdmin):
-    exclude = ('content_type', 'object_id')
-    readonly_fields = ('updated_by', 'last_updated', 'data_object_link')
-    list_display = ('name', 'data_object', 'severity', 'last_updated')
+    readonly_fields = ('updated_by', 'last_updated', 'linked_objects')
+    list_display = ('name', 'severity', 'last_updated')
 
-    def data_object_link(self, instance):
-        url = reverse('admin:%s_%s_change' % (instance.content_type.app_label, instance.content_type.model),
-                args=(instance.data_object.id,))
-        return format_html(
-                '<a href="{0}">{1}</a>',
-                url,
-                instance.data_object,
-                )
-
-    data_object_link.short_description = 'data object'
+    @classmethod
+    def linked_objects(cls, issue):
+        return list(issue.object_issues.all()) + list(issue.component_issues.all())
 
     def has_add_permission(self, request, obj=None):
         return False
 
 
-for _, cls in models.all_models.items():
-    if isinstance(cls, models.Object):
-        admin.site.register(cls, ObjectAdmin)
-    elif isinstance(cls, models.Issue):
+DISPLAY_FIELDS = {
+    'Object': ('name', 'updated_by', 'last_updated'),
+    'ObjectComponent': ('name', 'updated_by', 'last_updated',),
+    'CodeRun': ('run_identifier', 'updated_by', 'last_updated',),
+    'StorageRoot': ('name', 'updated_by', 'last_updated',),
+    'StorageLocation': ('path', 'updated_by', 'last_updated',),
+    'Source': ('name', 'updated_by', 'last_updated',),
+    'ExternalObject': ('doi_or_unique_name', 'updated_by', 'last_updated',),
+    'QualityControlled': ('object', 'updated_by', 'last_updated',),
+    'Keyword': ('object', 'keyphrase', 'updated_by', 'last_updated',),
+    'Author': ('object', 'family_name', 'personal_name', 'updated_by', 'last_updated',),
+    'Licence': ('object', 'updated_by', 'last_updated',),
+    'Namespace': ('name', 'updated_by', 'last_updated',),
+    'DataProduct': ('namespace', 'name', 'updated_by', 'last_updated',),
+    'CodeRepoRelease': ('name', 'version', 'updated_by', 'last_updated',),
+    'KeyValue': ('object', 'key', 'updated_by', 'last_updated',),
+}
+
+
+for name, cls in models.all_models.items():
+    if issubclass(cls, models.Issue):
         admin.site.register(cls, IssueAdmin)
     else:
-        admin.site.register(cls, BaseAdmin)
+        data = {'list_display': DISPLAY_FIELDS[name]}
+        admin_cls = type(name + 'Admin', (BaseAdmin,), data)
+        admin.site.register(cls, admin_cls)
