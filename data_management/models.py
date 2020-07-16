44b758a6ed9371b7662d5d54ec3f98ea01f5c277
Jonathan Hollocombe <jhollocombe@gmail.com>
2020-07-16 10:08:22 +0000

    Fixing Issue web page

--- data_management/models.py
+++ data_management/models.py
@@ -626,7 +626,7 @@ class DataProduct(BaseModel):
     object = models.OneToOneField(Object, on_delete=models.CASCADE, related_name='data_product')
     namespace = models.ForeignKey(Namespace, on_delete=models.CASCADE, related_name='data_products')
     name = NameField(null=False, blank=False)
-    version = VersionField()
+    version = models.TextField(max_length=TEXT_FIELD_LENGTH, null=True, blank=True)
 
     class Meta:
         constraints = [
