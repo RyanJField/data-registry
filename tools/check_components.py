8773529e91de7309580ac15ffed96baeb95eb9c8
Jonathan Hollocombe <Jonathan.Hollocombe@ukaea.uk>
2020-07-31 15:17:42 +0100

    Refactoring check_components.py and adding ability to find object by file hash.

--- tools/check_components.py
+++ tools/check_components.py
@@ -4,10 +4,13 @@ import toml
 import argparse
 import requests
 import html
+import hashlib
 
 API_ROOT = 'https://data.scrc.uk/api/'
 DATA_PRODUCT_URL = 'data_product/'
 NAMESPACE_URL = 'namespace/'
+STORAGE_LOCATION_URL = 'storage_location/'
+OBJECT_URL = 'object/'
 
 
 def is_leaf(node):
@@ -49,80 +52,113 @@ def url_to_id(url):
     return int(url.split('/')[-2])
 
 
-def main(args=None):
-    if args is None:
-        import sys
-        args = sys.argv[1:]
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument('data_product', type=str, help='Registry data product to look up')
-    parser.add_argument('file', type=str, help='File to process')
-    parser.add_argument('-n', '--namespace', type=str, help='Namespace of data product')
-
-    opts = parser.parse_args(args)
-    components = process(opts.file)
-
-    namespace_id = 0
-    if opts.namespace:
-        url = API_ROOT + NAMESPACE_URL + '?format=json&name=%s' % html.escape(opts.namespace)
-
-        r = requests.get(url)
-        if r.status_code != 200:
-            raise Exception(r.content)
-
-        result = r.json()
-        if not result or result['count'] == 0:
-            raise Exception('Could not namespace ' % opts.namespace)
-        if result['count'] > 1:
-            raise Exception('Multiple namespaces found for filter ' % opts.namespace)
-
-        namespace_id = url_to_id(result['results'][0]['url'])
-
-    url = API_ROOT + DATA_PRODUCT_URL + '?format=json&name=%s' % html.escape(opts.data_product)
-    if opts.namespace:
-        url += '&namespace=%d' % namespace_id
-
+def read_api(url, error_message):
     r = requests.get(url)
+
     if r.status_code != 200:
         raise Exception(r.content)
 
-    result = r.json()
-    if not result or result['count'] == 0:
-        raise Exception('Could not find data product ' % opts.data_product)
+    data = r.json()
 
-    data_product = result['results'][0]
-    object_url = data_product['object']
+    if not data:
+        raise Exception(error_message)
 
-    r = requests.get(object_url)
-    if r.status_code != 200:
-        raise Exception(r.content)
+    if 'count' in data:
+        if data['count'] == 0:
+            raise Exception(error_message)
+        return data['results'][0]
 
-    result = r.json()
-    component_urls = result['components']
+    return data
 
+
+def get_component_names(component_urls):
     names = []
-
     for url in component_urls:
         r = requests.get(url)
         if r.status_code != 200:
             raise Exception(r.content)
         result = r.json()
         names.append(result['name'])
+    return names
 
-    success = True
+
+def find_object_by_file_hash(file_hash, opts):
+    url = API_ROOT + STORAGE_LOCATION_URL + '?format=json&hash=%s' % html.escape(file_hash)
+
+    result = read_api(url, 'Could not find storage location with file hash matching %s' % opts.file)
+    storage_location_id = url_to_id(result['url'])
+
+    url = API_ROOT + OBJECT_URL + '?format=json&storage_location=%d' % storage_location_id
+
+    return read_api(url, 'Could not find object with storage_location %d' % storage_location_id)
+
+
+def find_object_by_data_product(opts):
+    url = API_ROOT + DATA_PRODUCT_URL + '?format=json&name=%s' % html.escape(opts.data_product)
+    if opts.namespace:
+        namespace_id = find_namespace_id(opts)
+        url += '&namespace=%d' % namespace_id
+
+    data_product = read_api(url, 'Could not find data product %s' % opts.data_product)
+    object_url = data_product['object']
+
+    return read_api(object_url, 'Failed to read object_url %s' % object_url)
+
+
+def find_namespace_id(opts):
+    url = API_ROOT + NAMESPACE_URL + '?format=json&name=%s' % html.escape(opts.namespace)
+    namespace = read_api(url, 'Could not find namespace %s' % opts.namespace)
+    return url_to_id(namespace['url'])
+
+
+def main(args=None):
+    if args is None:
+        import sys
+        args = sys.argv[1:]
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--data_product', type=str, help='Registry data product to look up')
+    parser.add_argument('file', type=str, help='File to process')
+    parser.add_argument('-n', '--namespace', type=str, help='Namespace of data product')
+    parser.add_argument('-v', '--verbose', action='store_true', help='Print verbose output')
+
+    opts = parser.parse_args(args)
+    components = process(opts.file)
+
+    if opts.data_product:
+        object_data = find_object_by_data_product(opts)
+    else:
+        with open(opts.file, 'rb') as f:
+            bytes = f.read()
+        file_hash = hashlib.sha1(bytes)
+        object_data = find_object_by_file_hash(file_hash.hexdigest(), opts)
+
+    component_urls = object_data['components']
+
+    names = get_component_names(component_urls)
+
+    if opts.verbose:
+        print('File components:')
+        for c in components:
+            print(c)
+        print()
+
+    errors = []
 
     for c in components:
         if c not in names:
-            print('File component %s not found in registry' % c)
-            success = False
+            errors.append('File component %s not found in registry' % c)
 
     for n in names:
         if n not in components:
-            print('Registry component %s not found in file' % n)
-            success = False
+            errors.append('Registry component %s not found in file' % n)
 
-    if success:
+    if not errors:
         print('All components match those in database')
+    else:
+        print('Errors found:')
+        for e in errors:
+            print(e)
 
 
 if __name__ == '__main__':
