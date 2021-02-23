20022d0db3fda4112c83c6ac984b9d1dd4ee0501
Andrew Lahiff <andrew.lahiff@gmail.com>
2021-02-23 11:54:26 +0000

    Read config from settings

--- data_management/object_storage.py
+++ data_management/object_storage.py
@@ -1,16 +1,15 @@
-from configparser import ConfigParser
 from hashlib import sha1
 import hmac
 import time
 
+from . import settings
+
 def create_url(path, method):
-    config = ConfigParser()
-    config.read('/home/ubuntu/config.ini')
-    expiry_time = int(time.time()) + int(config['storage']['duration'])
-    path = '/v1/' + config['storage']['bucket'] + '/' + path
+    expiry_time = int(time.time() + int(settings.CONFIG['SWIFT_DURATION']))
+    path = '/v1/' + settings.CONFIG['SWIFT_BUCKET'] + '/' + path
     if method == 'GET':
         hmac_body = '%s\n%s\n%s' % ('GET', expiry_time, path)
     elif method == 'PUT':
         hmac_body = '%s\n%s\n%s' % ('PUT', expiry_time, path)
-    sig = hmac.new(config['storage']['key'].encode('utf-8'), hmac_body.encode('utf-8'), sha1).hexdigest()
-    return '%s%s?temp_url_sig=%s&temp_url_expires=%d' % (config['storage']['url'], path, sig, expiry_time)
+    sig = hmac.new(settings.CONFIG['SWIFT_KEY'].encode('utf-8'), hmac_body.encode('utf-8'), sha1).hexdigest()
+    return '%s%s?temp_url_sig=%s&temp_url_expires=%d' % (settings.CONFIG['SWIFT_URL'], path, sig, expiry_time)
