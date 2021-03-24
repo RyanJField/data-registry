218b796af657902e680a63e80ad3d8a59d2910a3
Nathan Cummings <nathan.cummings@ukaea.uk>
2021-03-24 16:56:38 +0000

    Automated creation of local registry and user.

--- rebuild-local.sh
+++ rebuild-local.sh
@@ -1,8 +1,5 @@
 find . -path '*/migrations/*.py*' -not -path './venv/*' -not -name '__init__.py' -delete
 rm db.sqlite3
-export DJANGO_SETTINGS_MODULE="drams.local-settings"
-export DJANGO_SUPERUSER_USERNAME=admin
-export DJANGO_SUPERUSER_PASSWORD=admin
 python3 manage.py makemigrations custom_user
 python3 manage.py makemigrations data_management
 python3 manage.py migrate
---
+++ run_registry.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+mkdir ~/.scrc
+export SCRC_HOME=~/.scrc
+git clone https://github.com/ScottishCovidResponse/data-registry.git $SCRC_HOME
+python3 -m venv $SCRC_HOME/venv
+source $SCRC_HOME/venv/bin/activate
+pip install -r local-requirements.txt
+export DJANGO_SETTINGS_MODULE="drams.local-settings"
+export DJANGO_SUPERUSER_USERNAME=admin
+export DJANGO_SUPERUSER_PASSWORD=admin
+cd $SCRC_HOME
+./rebuild-local.sh
