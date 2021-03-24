1f30c2781a10c464bc6a0f39a48a78233db71169
Nathan Cummings <nathan.cummings@ukaea.uk>
2021-03-24 17:08:26 +0000

    update run_registry.sh

--- run_registry.sh
+++ run_registry.sh
@@ -4,7 +4,7 @@ export SCRC_HOME=~/.scrc
 git clone -b local-registry https://github.com/ScottishCovidResponse/data-registry.git $SCRC_HOME
 python3 -m venv $SCRC_HOME/venv
 source $SCRC_HOME/venv/bin/activate
-pip install -r local-requirements.txt
+pip install -r $SCRC_HOME/local-requirements.txt
 export DJANGO_SETTINGS_MODULE="drams.local-settings"
 export DJANGO_SUPERUSER_USERNAME=admin
 export DJANGO_SUPERUSER_PASSWORD=admin
