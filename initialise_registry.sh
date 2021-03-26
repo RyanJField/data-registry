02017452e08283152fb26b12ed660e4d897ef1f8
Nathan Cummings <nathan.cummings@ukaea.uk>
2021-03-26 14:11:02 +0000

    updating shell scripts

--- initialise_registry.sh
+++ initialise_registry.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-mkdir ~/.scrc
-export SCRC_HOME=~/.scrc
-git clone -b local-registry https://github.com/ScottishCovidResponse/data-registry.git $SCRC_HOME
-python3 -m venv $SCRC_HOME/venv
-source $SCRC_HOME/venv/bin/activate
-python -m pip install -r $SCRC_HOME/local-requirements.txt
+export SCRC_HOME="{$SCRC_HOME:-$HOME/.scrc}" >> "$HOME"/.profile
+mkdir "$SCRC_HOME"
+git clone -b local-registry https://github.com/ScottishCovidResponse/data-registry.git "$SCRC_HOME"
+python3 -m venv "$SCRC_HOME"/venv
+source "$SCRC_HOME"/venv/bin/activate
+python -m pip install -r "$SCRC_HOME"/local-requirements.txt
 export DJANGO_SETTINGS_MODULE="drams.local-settings"
 export DJANGO_SUPERUSER_USERNAME=admin
 export DJANGO_SUPERUSER_PASSWORD=admin
-cd $SCRC_HOME || exit
+cd "$SCRC_HOME" || exit
 ./rebuild-local.sh
