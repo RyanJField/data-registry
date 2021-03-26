3c0d231cc0a579f79f43c4bda5f6d701b3e0ef3d
Nathan Cummings <nathan.cummings@ukaea.uk>
2021-03-26 15:40:43 +0000

    updating shell scripts

--- scripts/rebuild-local.sh
+++ scripts/rebuild-local.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-find .. -path '*/migrations/*.py*' -not -path './venv/*' -not -name '__init__.py' -delete
-pwd
-rm ../db.sqlite3
-python3 ../manage.py makemigrations custom_user
-python3 ../manage.py makemigrations data_management
-python3 ../manage.py migrate
-python3 ../manage.py createsuperuser --noinput
+cd ..
+find . -path '*/migrations/*.py*' -not -path './venv/*' -not -name '__init__.py' -delete
+rm db.sqlite3
+python3 manage.py makemigrations custom_user
+python3 manage.py makemigrations data_management
+python3 manage.py migrate
+python3 manage.py createsuperuser --noinput
