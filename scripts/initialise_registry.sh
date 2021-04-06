436e6f59cd413a659ca7d7b92c66f1c3abc8e98f
nathan-cummings-ukaea <nathan.cummings@ukaea.uk>
2021-04-06 16:32:50 +0100

    Update initialisation script to improve speed

--- scripts/initialise_registry.sh
+++ scripts/initialise_registry.sh
@@ -4,6 +4,7 @@ mkdir "$SCRC_HOME"
 git clone -b local-registry https://github.com/ScottishCovidResponse/data-registry.git "$SCRC_HOME"
 python3 -m venv "$SCRC_HOME"/venv
 source "$SCRC_HOME"/venv/bin/activate
+python -m pip install --upgrade pip wheel
 python -m pip install -r "$SCRC_HOME"/local-requirements.txt
 export DJANGO_SETTINGS_MODULE="drams.local-settings"
 export DJANGO_SUPERUSER_USERNAME=admin
