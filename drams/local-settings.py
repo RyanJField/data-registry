aae8a18441b13693d61605e9286bce36a41684ec
nathan-cummings-ukaea <55384390+nathan-cummings-ukaea@users.noreply.github.com>
2021-04-20 15:18:21 +0100

    Change DEBUG to True in local-settings.py
    This is because django will not serve static files in production (DEBUG=False). This should be the job of the web server (in this case Nginx). Since the local registry isn't using a proper web server and is HTTP only, we are already in "grossly inefficient and probably insecure" territory so having DEBUG set to 'True' seems like the best option right now.

--- drams/local-settings.py
+++ drams/local-settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '91qh0bw%vj8jd(+s1dos++=thx3v165*jlejlt9l-e&2b1*@ak'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 # DEBUG_PROPAGATE_EXCEPTIONS = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
