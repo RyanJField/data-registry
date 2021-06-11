0deee89273550bd07695ba6effc01e9889a194ea
Nathan Cummings <nathan.cummings@ukaea.uk>
2021-06-11 08:38:58 +0100

    Add steps to build schema diagram to install

--- scripts/rebuild-local.sh
+++ scripts/rebuild-local.sh
@@ -5,5 +5,7 @@ rm db.sqlite3
 python3 manage.py makemigrations custom_user
 python3 manage.py makemigrations data_management
 python3 manage.py migrate
+python3 manage.py graph_models data_management --arrow-shape crow -X "BaseModel,DataObject,DataObjectVersion" -E -o schema.dot
+dot schema.dot -Tsvg -o static/images/schema.svg
 python3 manage.py collectstatic --noinput
 python3 manage.py createsuperuser --noinput
