120ead2c3531c24e07b99b4b5e62453c1d1220ab
Andrew Lahiff <andrew.lahiff@gmail.com>
2021-06-18 10:29:58 +0000

    Add CLI comment for creating a file containing a token

---
+++ data_management/management/commands/get_token.py
@@ -0,0 +1,16 @@
+import os 
+
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
+
+class Command(BaseCommand):
+    def handle(self, **options):
+        user_model = get_user_model()
+        user = user_model.objects.get_by_natural_key('admin')
+        token = Token.objects.get_or_create(user=user)
+
+        token_filename = os.path.join(os.environ['SCRC_HOME'], 'token')
+        with open(token_filename, 'w') as fh:
+            fh.write(str(token[0]))
+        os.chmod(token_filename, 0o600)
