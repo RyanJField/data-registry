0360636b629c91c4d93b8e8807e56bb853a8b329
Nathan Cummings <nathan.cummings@ukaea.uk>
2021-07-14 15:22:14 +0100

    Allow both identifier and other_unique_name to be set for ExternalObject

--- data_management/models.py
+++ data_management/models.py
@@ -670,6 +670,7 @@ class ExternalObject(BaseModel):
                 check=(
                     models.Q(identifier__isnull=True, other_unique_name__isnull=False)
                     | models.Q(identifier__isnull=False, other_unique_name__isnull=True)
+                    | models.Q(identifier__isnull=False, other_unique_name__isnull=False)
                 ),
             )
         ]
